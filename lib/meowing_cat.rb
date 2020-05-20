7bd05b1587d50e1638318e6286f10e417d856601
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-20 18:43:30 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,6 +1,11 @@
 class Cat
+<<<<<<< HEAD
   attr_accessor :cats, :name, :meow
   def meow
     puts "meow!"
   end
+=======
+  
+  def attr_accessor name
+>>>>>>> a388096c614203caec7eb14dcca526a8c44a96bb
 end
